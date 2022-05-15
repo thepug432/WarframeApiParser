d17c355dd88bcc893084b1a93a4093ab438b982c
Kiril <alexbour82@gmail.com>
2022-05-15 12:54:12 -0300

    added context providers

--- src/hooks/HideProvider.jsx
+++ src/hooks/HideProvider.jsx
@@ -6,6 +6,26 @@ const GetNews = createContext();
 const GetArb = createContext();
 const GetAlerts = createContext();
 const GetWave = createContext();
+const GetSortie = createContext();
+const GetInvasions = createContext();
+const GetFissures = createContext();
+const GetSales = createContext();
+
+export const UseSortie = () => {
+    return useContext(GetSortie);
+};
+
+export const UseInvasions = () => {
+    return useContext(GetInvasions);
+};
+
+export const UseFissures = () => {
+    return useContext(GetFissures);
+};
+
+export const UseSales = () => {
+    return useContext(GetSales);
+};
 
 export const UseGetCycle = () => {
     return useContext(GetCycle);
@@ -38,19 +58,31 @@ export default function HideProvider({children}){
     const [seeArb, changeSeeArb] = useState(true);
     const [seeAlerts, changeSeeAlerts] = useState(true);
     const [getWave, changeGetWave] = useState(true);
+    const [getSortie, changeGetSortie] = useState(true);
+    const [getInvasions, changeGetInvasions] = useState(true);
+    const [getFissures, changeGetFissures] = useState(true);
+    const [getSales, changeGetSales] = useState(true);
 
     return(
         <GetCycle.Provider value={[seeCycle, changeSeeCycle]}>
         <GetEvents.Provider value={[seeEvents, changeSeeEvents]}>
-            <GetNews.Provider value={[seeNews, changeSeeNews]}>
-                <GetArb.Provider value={[seeArb, changeSeeArb]}>
-                    <GetAlerts.Provider value={[seeAlerts, changeSeeAlerts]}>
-                        <GetWave.Provider value={[getWave, changeGetWave]}>
+        <GetNews.Provider value={[seeNews, changeSeeNews]}>
+        <GetArb.Provider value={[seeArb, changeSeeArb]}>
+        <GetAlerts.Provider value={[seeAlerts, changeSeeAlerts]}>
+        <GetWave.Provider value={[getWave, changeGetWave]}>
+            <GetSortie.Provider value={[getSortie, changeGetSortie]}>               
+                <GetInvasions.Provider value={[getInvasions, changeGetInvasions]}>             
+                    <GetFissures.Provider value={[getFissures, changeGetFissures]}>       
+                        <GetSales.Provider value={[getSales, changeGetSales]}>
                             {children}
-                        </GetWave.Provider>
-                    </GetAlerts.Provider>
-                </GetArb.Provider>
-             </GetNews.Provider>
+                        </GetSales.Provider>
+                    </GetFissures.Provider>   
+                </GetInvasions.Provider>   
+            </GetSortie.Provider> 
+        </GetWave.Provider>
+        </GetAlerts.Provider>
+        </GetArb.Provider>  
+        </GetNews.Provider>
         </GetEvents.Provider>
         </GetCycle.Provider>
     );
