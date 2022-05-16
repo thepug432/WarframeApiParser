9500cc5ace4e1e3be9ccb44e873aa02350091044
Kiril <alexbour82@gmail.com>
2022-05-16 18:22:04 -0300

    added title on hover

--- src/components/nav/Hide.jsx
+++ src/components/nav/Hide.jsx
@@ -1,7 +1,8 @@
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 export default function Hide({head, li, title}) {
-
+    const [hover, switchHover] = useState(false)
     const hide = (e, parm) => {
         if (parm[0]){
             e.currentTarget.firstChild.firstChild.setAttribute("fill", "gray")
@@ -13,8 +14,9 @@ export default function Hide({head, li, title}) {
 
     return(
         <motion.td 
+            onHoverStart={() => switchHover(true)}
+            onHoverEnd={() => switchHover(false)}
             whileHover={{ scale: 1.2 }} 
-            title={title} 
             className="p-3" 
             onClick={(e) => hide(e, li)}
             initial={{ scale: 0 }} 
@@ -22,6 +24,20 @@ export default function Hide({head, li, title}) {
             exit={{ scale: 0 }}
             >
             <h1>{head}</h1>
+            <AnimatePresence>
+                {hover &&
+                    <>
+                        <div className="absolute top-8 right-5 border-solid border-slate-900 border-b-8 border-x-transparent border-x-8 border-t-0"></div>
+                        <motion.div 
+                        initial={{ scale: 0 }} 
+                        animate={{ scale: 1 }}
+                        exit={{ scale: 0 }}
+                        className="absolute top-10 right-4 bg-slate-900 text-white p-2 rounded text-sm">
+                            <p>{title}</p>
+                        </motion.div>
+                    </>
+                }
+            </AnimatePresence>
         </motion.td>
     )
 }
